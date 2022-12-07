2857084c27bef7beedd90188eaf16de7e03cfb3f
Itz_me_Hazel <deaese94@gmail.com>
2022-12-07 18:27:15 +0900

    JS: remove active from nav

--- main.js
+++ main.js
@@ -60,8 +60,15 @@ workBtnContainer.addEventListener("click", (e) => {
   if (filter == null) {
     return;
   }
-  projectBtnContainer.classList.add("anim-out");
 
+  //Remove selection from the previous item and slect the new one
+  const active = document.querySelector(".category__btn.selected");
+  active.classList.remove("selected");
+  const target =
+    e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
+  target.classList.add("selected");
+
+  projectBtnContainer.classList.add("anim-out");
   setTimeout(() => {
     Projects.forEach((project) => {
       console.log(project.dataset.type);
